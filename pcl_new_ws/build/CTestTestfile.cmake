d0a4e673c8a99dda2b6b972d36d72b955fd1dbd4
junkdood <1354784375@qq.com>
2020-12-16 16:25:35 +0800

    rm

--- pcl_new_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/d402/pcl_new_ws/src
-# Build directory: /home/d402/pcl_new_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("pcl_test")
--- pcl_new_ws/build/pcl_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/d402/pcl_new_ws/src/pcl_test
-# Build directory: /home/d402/pcl_new_ws/build/pcl_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
